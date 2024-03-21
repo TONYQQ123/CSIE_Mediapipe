7cfb8d1daa019ac5e1fa8b0e0814c593a8b8df8c
TONYQQ123 <tony0910242070@gmail.com>
2024-03-21 12:34:52 +0800

    merge lab

---
+++ ui/lib/share.py
@@ -0,0 +1,9 @@
+class Share_Info:
+    Home_Window = None
+    Login_Window = None
+    Sign_up_Window = None
+    Information_Window = None
+    Update_Information_Window = None
+    Rank_Window = None
+    Run_detect_Window = None
+    Activity_Window = None
